7e09952d73e967bac2cfbb7b46704b7f5da84d00
Rene Korss <rene.korss@gmail.com>
2016-02-19 13:29:41 +0200

    Add Nordea test

---
+++ tests/NordeaTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RKD\BanklinkTests;
+
+/**
+ * Test suite for Nordea banklink.
+ *
+ * @author  Rene Korss <rene.korss@gmail.com>
+ */
+class KrediidipankTest extends SEBTest
+{
+    protected $bankClass = "RKD\Banklink\Nordea";
+
+    protected $requestUrl = 'https://netbank.nordea.com/pnbepay/epayp.jsp';
+    protected $testRequestUrl = 'http://localhost:8080/banklink/ipizza';
+}
