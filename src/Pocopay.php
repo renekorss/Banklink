cb8befb6a5b618eebf3a02e9f732ef44a70cb7de
Rene Korss <rene.korss@gmail.com>
2018-06-22 08:40:51 +0300

    Add Pocopay support, closes #5

---
+++ src/Pocopay.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * RKD Banklink.
+ *
+ * @link https://github.com/renekorss/Banklink/
+ *
+ * @author Rene Korss <rene.korss@gmail.com>
+ * @copyright 2016-2018 Rene Korss
+ * @license MIT
+ */
+namespace RKD\Banklink;
+
+use RKD\Banklink\Protocol\IPizza;
+
+/**
+ * Banklink settings for Pocopay.
+ *
+ * For more information, please visit:
+ * https://pocopay.com/
+ *
+ * @author  Rene Korss <rene.korss@gmail.com>
+ */
+class Pocopay extends Banklink
+{
+    /**
+     * Request url.
+     *
+     * @var string
+     */
+    protected $requestUrl = 'https://my.pocopay.com/banklink';
+
+    /**
+     * Test request url.
+     * 
+     * @todo Add correct testing URL.
+     *
+     * @var string
+     */
+    protected $testRequestUrl = 'http://localhost:8080/banklink/seb-common';
+
+    /**
+     * Force SEB class to use IPizza protocol.
+     *
+     * @param RKD\Banklink\Protocol\IPizza $protocol   Protocol used
+     */
+    public function __construct(IPizza $protocol)
+    {
+        parent::__construct($protocol);
+    }
+
+    /**
+     * Override encoding field.
+     */
+    protected function getEncodingField()
+    {
+        return 'VK_ENCODING';
+    }
+
+    /**
+     * By default uses UTF-8.
+     *
+     * @return array Array of additional fields to send to bank
+     */
+    protected function getAdditionalFields()
+    {
+        return array(
+            'VK_ENCODING' => $this->requestEncoding,
+        );
+    }
+}
