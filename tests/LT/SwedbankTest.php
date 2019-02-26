40e1107d76629f6d3933d5b54ca6ebef29721c54
Rene Korss <rene.korss@gmail.com>
2019-02-26 14:03:28 +0200

    Add correct tests

--- tests/LT/SwedbankTest.php
+++ tests/LT/SwedbankTest.php
@@ -28,8 +28,35 @@ class SwedbankTest extends SEBTest
         parent::setUp();
 
         // Swedbank has encoding
+        $this->expectedData['VK_SERVICE'] = '1002';
         $this->expectedData['VK_ENCODING'] = 'UTF-8';
+        $this->expectedData['VK_MAC'] = 'M1EHQuM8AZ6bN+/RWeMUNIXe0bvWQB0ypVZhhoW1iz8VkE/a0OkjuqcqZRgEOdOyhs5GbP5Oy/PcqFV/rJ7yZIjXwsxM+6Ux+g4n7Xz088Jy0VuNFNvBUPf9yJOKW7Zjm3PNlqo3YrIB5rJpTb805MmMORl9iAYd7YywQLQrHL0=';
 
-        $this->setUpBanklinkWithSeller();
+        unset($this->expectedData['VK_NAME']);
+        unset($this->expectedData['VK_ACC']);
+
+
+        $this->setUpBanklink();
+    }
+
+    /**
+     * Test custom request data
+     */
+    public function testCustomRequestData()
+    {
+        $this->bank = new $this->bankClass($this->protocol);
+
+        $customData = [
+            'INAPP' => 1, // new data
+            'VK_REF' => 'mycustomref', // override data
+            'VK_MAC' => 'H99NKHaAoHr0Dmj4xJJBIVdiKkzWCMn5oUGKulTKWVgMrSFVFyUrXPVXTRl+t7f9PmQMYQ+LHbeENwhCiLGstlEjm53l61UD9pGMJv+YJFTOh2tOPifnzEE1wsck5wJlnOUY8yz64dGD2igR35S6GKdOyauie3MFcfEsFYSbkFw='
+        ];
+
+        $this->expectedData = array_merge($this->expectedData, $customData);
+
+        $request = $this->bank->getPaymentRequest($this->orderId, $this->amount, $this->message, $this->language, $this->currency, $customData, $this->timezone);
+
+        // Custom url
+        $this->assertEquals($this->expectedData, $request->getRequestData());
     }
 }
