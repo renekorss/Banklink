c9cefd853e34b26eecfc70cbb692283b26093928
Rene Korss <rene.korss@gmail.com>
2021-08-30 19:55:32 +0300

    Suppress more

--- tests/EE/CoopPankTest.php
+++ tests/EE/CoopPankTest.php
@@ -6,6 +6,9 @@ namespace RKD\Banklink\Test\EE;
  * Test suite for Coop Pank banklink.
  *
  * @author  Rene Korss <rene.korss@gmail.com>
+ *
+ * @SuppressWarnings(PHPMD.TooManyFields)
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
  */
 class CoopPankTest extends SEBTest
 {
